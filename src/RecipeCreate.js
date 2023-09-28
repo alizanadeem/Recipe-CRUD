24b4589d9d996c5da94f1f0f31317f785a3a0305
Aliza nadeem <alizanadeemch@gmail.com>
2023-09-28 10:52:59 -0500

    fixed duplicate recipes issues

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 
-function RecipeCreate({createRecipe}) {
-  const [name, setName] = useState(""); 
-  const [cuisine, setCuisine] = useState(""); 
-  const [photo, setPhoto] = useState(""); 
-  const [ingredients, setIngredients] = useState(""); 
-  const [preparation, setPreparation] = useState(""); 
+function RecipeCreate({ createRecipe }) {
+  const [name, setName] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [preparation, setPreparation] = useState("");
 
-
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     createRecipe({
       name,
@@ -23,15 +22,6 @@ function RecipeCreate({createRecipe}) {
     setIngredients("");
     setPreparation("");
   };
-  
-  
-  
-    
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
-   
-
 
   return (
     <form name="create" onSubmit={handleSubmit}>
@@ -39,38 +29,76 @@ function RecipeCreate({createRecipe}) {
         <tbody>
           <tr>
             <td>
-           <label htmlFor="name">
-<input type="text" id="name" name="name" value= {name} placeholder="Name" value ={name} onChange={(e) => setName(e.target.value)} />
-</label>
- </td>
-           
-            
-       <td>     
- <label htmlFor="cuisine">
-<input type="text" id="cuisine" name="cuisine" placeholder="cuisine" value={photo} onChange={(e) => setCuisine(e.target.value)}/>
-</label>
- </td>       
-      
-         <td>     
- <label htmlFor="photo">
-<input type="URL" id="photo" name="photo" placeholder="URL" value = {photo}  onChange={(e) => setPhoto(e.target.value)}/>
-</label>
- </td>       
-         
-         
-  <td>     
- <label htmlFor="ingredients">
-<textarea type="text" id="ingredients" name="ingredients" placeholder="ingredients" rows="4" cols="12"value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
-</label>
- </td>  
-         
-     <td>     
- <label htmlFor="preparation">
-<textarea type="text" id="preparation" name="preparation" placeholder="preparation" rows="4" cols="12" value={preparation} onChange={(e) => setPreparation(e.target.value)}/>
-</label>
- </td>         
+              <label htmlFor="name">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
+            </td>
+
             <td>
-              <button type="submit" onClick={createRecipe}>Create</button>
+              <label htmlFor="cuisine">
+                <input
+                  type="text"
+                  id="cuisine"
+                  name="cuisine"
+                  placeholder="cuisine"
+                  value={cuisine}
+                  onChange={(e) => setCuisine(e.target.value)}
+                />
+              </label>
+            </td>
+
+            <td>
+              <label htmlFor="photo">
+                <input
+                  type="URL"
+                  id="photo"
+                  name="photo"
+                  placeholder="URL"
+                  value={photo}
+                  onChange={(e) => setPhoto(e.target.value)}
+                />
+              </label>
+            </td>
+
+            <td>
+              <label htmlFor="ingredients">
+                <textarea
+                  type="text"
+                  id="ingredients"
+                  name="ingredients"
+                  placeholder="ingredients"
+                  rows="4"
+                  cols="12"
+                  value={ingredients}
+                  onChange={(e) => setIngredients(e.target.value)}
+                />
+              </label>
+            </td>
+
+            <td>
+              <label htmlFor="preparation">
+                <textarea
+                  type="text"
+                  id="preparation"
+                  name="preparation"
+                  placeholder="preparation"
+                  rows="4"
+                  cols="12"
+                  value={preparation}
+                  onChange={(e) => setPreparation(e.target.value)}
+                />
+              </label>
+            </td>
+
+            <td>
+              <button type="submit">Create</button>
             </td>
           </tr>
         </tbody>
